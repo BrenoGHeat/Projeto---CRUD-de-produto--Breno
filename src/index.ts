abce18fe31395fc7f20f0310c4681ac3122d95f2
Breno Garcia <BrenoGHeat@gmail.com>
2024-09-11 14:07:44 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -6,31 +6,35 @@ class ProductList implements IProductFinally{
    
    id: number = 1;
 
-
-    createProduct(data:{name:string, price: number}){
+    createProduct(data:{ name:string, price: number }){
         const newProduct:IProduct = {...data, createdAt: new Date(), updatedAt: new Date() , id:this.id}
         this.productList.push(newProduct);
         this.id += 1;
         return newProduct;
     }
 
-
     getProducts(){
        return this.productList;
     }
 
-    
-    updateProduct(id: number, data: { name: string | undefined; price: number | undefined; }): IProduct{
-        this.productList.splice(id, 1);
-
-
+    getOneProduct(id: number): IProduct | undefined {
+        const oneProductId = this.productList.find(product => product.id === id);
+        return oneProductId;
     }
 
-   deleteProduct(id: number): { message: string} {
+    updateProduct(id: number, data: { name?: string ; price?: number }): IProduct {
+        const index = this.productList.findIndex(product => product.id === id);
+        const newProduct : IProduct = {...this.productList[index], updatedAt: new Date() , ...data}
+        this.productList.splice( index, 1, newProduct )
+        return newProduct;
+    }
+
+   deleteProduct(id: number): { message: string } {
         const index = this.productList.findIndex(product => product.id === id);
         this.productList.splice(index, 1);
         return { message: "Product successfully deleted."}
    }
-
-
+      
 }
+
+export const productList = new ProductList();
