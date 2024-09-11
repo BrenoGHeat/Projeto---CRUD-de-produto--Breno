aeab0cf906db1f5c483307c07a7bb60fb43024f0
Breno Garcia <BrenoGHeat@gmail.com>
2024-09-11 11:26:10 -0300

    updates

--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,26 @@ class ProductList implements IProductFinally{
         const newProduct:IProduct = {...data, createdAt: new Date(), updatedAt: new Date() , id:this.id}
         this.productList.push(newProduct);
         this.id += 1;
-        return newProduct
+        return newProduct;
     }
 
 
     getProducts(){
        return this.productList;
     }
+
+    
+    updateProduct(id: number, data: { name: string | undefined; price: number | undefined; }): IProduct{
+        this.productList.splice(id, 1);
+
+
+    }
+
+   deleteProduct(id: number): { message: string} {
+        const index = this.productList.findIndex(product => product.id === id);
+        this.productList.splice(index, 1);
+        return { message: "Product successfully deleted."}
+   }
 
 
 }
